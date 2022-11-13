1414728c3c15250e127fc4d745284654b99b0d3f
Bryce Roth <broth7@cnm.edu>
2022-11-13 13:58:07 -0700

    checking library table and files

--- backend/src/utils/models/Library.ts
+++ backend/src/utils/models/Library.ts
@@ -1,4 +1,4 @@
-import {sql} from '../database.utils';
+import {sql} from '../database.utils'
 
 export interface Library {
     libraryId: string|null
@@ -29,26 +29,26 @@ export async function insertLibrary (library: Library): Promise<string> {
     return 'Library created successfully!'
 }
 
-export async function selectAllLibraries (): Promise<Library[]> {
-    return <Library[]> await sql `SELECT library_id, library_profile_id, library_address, library_description, library_event_opt_in, library_lat, library_lng, library_name, library_specialization, library_type FROM library ORDER BY library_id DESC`
-}
-
-export async function selectLibraryByLibraryId (libraryId: string): Promise<Library | null> {
-    const result = <Library[]> await sql `SELECT library_id, library_profile_id, library_address, library_description, library_event_opt_in, library_lat, library_lng, library_name, library_specialization, library_type FROM library WHERE library_id = ${libraryId}`
-    return result?.length === 1 ? result[0] : null
-}
-
-export async function selectLibrariesByLibraryProfileId (libraryProfileId: string): Promise<Library[]> {
-    return <Library[]> await sql `SELECT library_id, library_profile_id, library_address, library_description, library_event_opt_in, library_lat, library_lng, library_name, library_specialization, library_type FROM library WHERE library_profile_id = ${libraryProfileId}`
-}
-
-export async function selectPartialLibraryByLibraryId (libraryId: string): Promise<Library[]> {
-    return <Library[]> await sql `SELECT library_id, library_address, library_description, library_name, library_specialization FROM library WHERE library_id = ${libraryId}`
-}
-
-export async function selectPartialLibraryByLibraryIdAndLibraryProfileId (libraryId: string, libraryProfileId: string): Promise<Library[]> {
-    return <Library[]> await sql `SELECT library_id, library_profile_id, library_address, library_description, library_name, library_specialization, library_type FROM library WHERE library_id = {${libraryId} AND library_profile_id = ${libraryProfileId}`
-}
-
-export async function updateLibrary (libraryId: string, libraryProfileId: string): Promise<Library[]> {
-    return <Library[]> await sql `UPDATE library SET library_address = :libraryAddress, library_description = :libraryDescription, library_event_opt_in = :libraryEventOptIn, library_name = :libraryName, library_specialization = :librarySpecialization WHERE library_id = ${library_id} AND library_profile_id = ${library_profile_id}`
+// export async function selectAllLibraries (): Promise<Library[]> {
+//     return <Library[]> await sql `SELECT library_id, library_profile_id, library_address, library_description, library_event_opt_in, library_lat, library_lng, library_name, library_specialization, library_type FROM library ORDER BY library_id DESC`
+// }
+//
+// export async function selectLibraryByLibraryId (libraryId: string): Promise<Library | null> {
+//     const result = <Library[]> await sql `SELECT library_id, library_profile_id, library_address, library_description, library_event_opt_in, library_lat, library_lng, library_name, library_specialization, library_type FROM library WHERE library_id = ${libraryId}`
+//     return result?.length === 1 ? result[0] : null
+// }
+//
+// export async function selectLibrariesByLibraryProfileId (libraryProfileId: string): Promise<Library[]> {
+//     return <Library[]> await sql `SELECT library_id, library_profile_id, library_address, library_description, library_event_opt_in, library_lat, library_lng, library_name, library_specialization, library_type FROM library WHERE library_profile_id = ${libraryProfileId}`
+// }
+//
+// export async function selectPartialLibraryByLibraryId (libraryId: string): Promise<Library[]> {
+//     return <Library[]> await sql `SELECT library_id, library_address, library_description, library_name, library_specialization FROM library WHERE library_id = ${libraryId}`
+// }
+//
+// export async function selectPartialLibraryByLibraryIdAndLibraryProfileId (libraryId: string, libraryProfileId: string): Promise<Library[]> {
+//     return <Library[]> await sql `SELECT library_id, library_profile_id, library_address, library_description, library_name, library_specialization, library_type FROM library WHERE library_id = {${libraryId} AND library_profile_id = ${libraryProfileId}`
+// }
+//
+// export async function updateLibrary (libraryId: string, libraryProfileId: string): Promise<Library[]> {
+//     return <Library[]> await sql `UPDATE library SET library_address = :libraryAddress, library_description = :libraryDescription, library_event_opt_in = :libraryEventOptIn, library_name = :libraryName, library_specialization = :librarySpecialization WHERE library_id = ${library_id} AND library_profile_id = ${library_profile_id}`
