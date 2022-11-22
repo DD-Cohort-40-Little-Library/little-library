3fc9107d2ce2df1e3fe76ceb3b5f37d4b0e7b7f1
Kelly Bowman <KBowman5@CNM.Edu>
2022-11-21 17:27:25 -0700

    mostly good to go, just working on libraryProfileId extraction for data-downloader

--- backend/src/utils/data-downloader/data-downloader.ts
+++ backend/src/utils/data-downloader/data-downloader.ts
@@ -29,47 +29,64 @@ function LLibraryDataDownloader() : Promise<any> {
                 .on('data', (data: any) => results.push(data))
                 .on('end',  async () => {
                     try {
-                        //A fake profile must be created to own the tweets being imported for the data downloader
-                        const profileHash = await setHash("ILikeFakePasswordsWithNoSpaces");
-                        const profile: Profile = {
+                        //A fake profile must be created to own the libraries being imported for the data downloader
+                        const profileHash1 = await setHash("ILikeFakePasswordsWithNoSpaces");
+                        const profile1: Profile = {
                             profileId: uuid(),
                             profileActivationToken: null,
                             profileAvatarUrl: "http://www.fillmurray.com/150/150",
                             profileEmail: "mxFakeAccount+1@fake-acounts.rus",
                             profileFirstName: "Private",
-                            profileHash,
+                            profileHash: profileHash1,
                             profileLastName: "Libraries",
                             profileName: "MurrayTheFirst"
-                        },
-                        {
-                            //A fake profile must be created to own the tweets being imported for the data downloader
-                            const profileHash = await setHash("SecondILikeFakePasswordsWithNoSpaces");
-                            const profile: Profile = {
+                        }
+                        const profileHash2 = await setHash("SecondILikeFakePasswordsWithNoSpaces");
+                        const profile2: Profile = {
                             profileId: uuid(),
                             profileActivationToken: null,
                             profileAvatarUrl: "http://www.fillmurray2.com/150/150",
                             profileEmail: "SecondMxFakeAccount+1@fake-acounts.rus",
                             profileFirstName: "Public",
-                            profileHash,
+                            profileHash: profileHash2,
                             profileLastName: "Libraries",
                             profileName: "MurrayTheSecond"
                         }
-                        console.log(await insertProfile(profile))
+                        console.log(await insertProfile(profile1))
+                        console.log(await insertProfile(profile2))
+
                         for (let result of results) {
 
-                            ****************** IFIFIFIFIFFIFif
-                            const library: Library = {
-                                libraryId: null,
-                                libraryProfileId: profile.profileId,
-                                libraryAddress: result['library_address'],
-                                libraryDescription: result['library_description'],
-                                libraryEventOptIn: result['library_event_opt_in'],
-                                libraryLat: result['library_lat'],
-                                libraryLng: result['library_lng'],
-                                libraryName: result['library_name'],
-                                librarySpecialization: result['library_specialization'],
-                                libraryType: result['library_type']
-                            }
+                            // ****************** IFIFIFIFIFFIFif
+                            const {libraryId, libraryProfileId, libraryAddress, libraryDescription, libraryEventOptIn, libraryLat, libraryLng, libraryName, librarySpecialization, libraryType} = result
+                                if (result.libraryType === 'Little Library') {
+                                    const library: Library = {
+                                        libraryId: null,
+                                        libraryProfileId: result.libraryProfileId,
+                                        libraryAddress: result['library_address'],
+                                        libraryDescription: result['library_description'],
+                                        libraryEventOptIn: result['library_event_opt_in'],
+                                        libraryLat: result['library_lat'],
+                                        libraryLng: result['library_lng'],
+                                        libraryName: result['library_name'],
+                                        librarySpecialization: result['library_specialization'],
+                                        libraryType: result['library_type']
+                                    }
+                                }
+                                else {
+                                    const library: Library = {
+                                        libraryId: null,
+                                        libraryProfileId: profile1.profileId,
+                                        libraryAddress: result['library_address'],
+                                        libraryDescription: result['library_description'],
+                                        libraryEventOptIn: result['library_event_opt_in'],
+                                        libraryLat: result['library_lat'],
+                                        libraryLng: result['library_lng'],
+                                        libraryName: result['library_name'],
+                                        librarySpecialization: result['library_specialization'],
+                                        libraryType: result['library_type']
+                                    }
+                                }
                             console.log(library)
                             console.log(await insertLibrary(library))
                         }
