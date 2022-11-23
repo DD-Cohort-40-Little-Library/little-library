93a1ca44575d9059dbdafe4bc48cf7d2fe9c5943
Bryce Roth <broth7@cnm.edu>
2022-11-23 11:50:16 -0700

    added in framework for main page

---
+++ frontend/src/ui/EventColumn.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import {Col, Container} from "react-bootstrap";
+
+export const EventList = () => {
+    return (
+        <>
+            <Container>
+                <Col className={}>
+                    <h2>Events</h2>
+                </Col>
+            </Container>
+        </>
+    )
+}
