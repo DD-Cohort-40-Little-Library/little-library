42dd62947165d8a511cd3520a1e9f09428836a13
Kelly Bowman <KBowman5@CNM.Edu>
2022-11-25 16:30:46 -0700

    got pin modal ready for data connections

--- frontend/src/ui/Home.jsx
+++ frontend/src/ui/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react"
+import {MapLibraryPinModal} from "./shared/components/map-library-pin-modal/MapLibraryPinModal";
 
 export function Home() {
 	return (
 		<>
 			<h1>Home</h1>
 			<h2>TEST 1</h2>
+			<p>Pretend there's a map here and a pin here <MapLibraryPinModal id="pin-modal"/> </p>
 		</>
 	)
 }
