41d526a0948a1da8aa0bd8e1b9713b54fe08f653
Nycole Davila <ndavila1@cnm.edu>
2022-12-01 10:09:32 -0700

    signupv.2

--- frontend/src/ui/shared/components/main-nav/sign-in/SignUpForm.jsx
+++ frontend/src/ui/shared/components/main-nav/sign-in/SignUpForm.jsx
@@ -1,9 +1,13 @@
 import React from "react";
-import {Formik} from "formik";
+import {Form, Formik} from "formik";
 import jwtDecode from "jwt-decode";
 import * as Yup from "yup";
 import {httpConfig} from "../../../utils/http-config.js";
 import {values} from "mapbox-gl/src/util/util.js";
+import {DisplayStatus} from "../../display-status/DisplayStatus";
+import {FormControl, InputGroup} from "react-bootstrap";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {DisplayError} from "../../display-error/DisplayError";
 
 
 export const SignUpForm = () => {
@@ -45,6 +49,149 @@ export const SignUpForm = () => {
             }
         )
     }
+    return (
+        <Formik
+            initialValues={signUp}
+            onSubmit={submitSignUp}
+            validationSchema={validator}
+            >
+            {SignUpFormContent}
+        </Formik>
+    )
+}
 
+function SignUpFormContent(props){
+    const {
+        status,
+        values,
+        errors,
+        touched,
+        dirty,
+        isSubmitting,
+        handleChange,
+        handleBlur,
+        handleSubmit,
+        handleReset
+    } = props
+    return (
+        <>
+            <Form onSubmit={handleSubmit}>
+                  <Form.Group controlId={'profileEmail'}>
+                      <Form.Label>Email</Form.Label>
+                      <InputGroup>
+                        <InputGroup.Text>
+                            Your Email
+                        </InputGroup.Text>
+                          <FormControl
+                              // className="form-control"
+                              name="profileEmail"
+                              type="text"
+                              value={values.profileEmail}
+                              placeholder="your@email.you"
+                              onChange={handleChange}
+                              onBlur={handleBlur}
+                          />
+                      </InputGroup>
+                      <DisplayError errors={errors} touched={touched} field={'profileEmail'}/>
+                  </Form.Group>
 
+                <Form.Group controlId={'profileFirstName'}>
+                    <Form.Label>First Name</Form.Label>
+                    <InputGroup>
+                        {/*<InputGroup.Text>*/}
+                        {/*    UserName*/}
+                        {/*</InputGroup.Text>*/}
+                        <FormControl
+                            // className="form-control"
+                            name='profileFirstName'
+                            type='text'
+                            value={values.profileFirstName}
+                            placeholder='First Name'
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                        />
+                    </InputGroup>
+                    <DisplayError errors={errors} touched={touched} field={'profileFirstName'}/>
+                </Form.Group>
+
+                <Form.Group controlId={'profileLastName'}>
+                    <Form.Label>Last Name</Form.Label>
+                    <InputGroup>
+                        {/*<InputGroup.Text>*/}
+                        {/*    UserName*/}
+                        {/*</InputGroup.Text>*/}
+                        <FormControl
+                            // className="form-control"
+                            name='profileLastName'
+                            type='text'
+                            value={values.profileLastName}
+                            placeholder='Last Name'
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                        />
+                    </InputGroup>
+                    <DisplayError errors={errors} touched={touched} field={'profileLastName'}/>
+                </Form.Group>
+
+                <Form.Group controlId={'profileName'}>
+                    <Form.Label>UserName</Form.Label>
+                    <InputGroup>
+                        {/*<InputGroup.Text>*/}
+                        {/*    UserName*/}
+                        {/*</InputGroup.Text>*/}
+                        <FormControl
+                            // className="form-control"
+                            name='profileName'
+                            type='text'
+                            value={values.profileName}
+                            placeholder='UserName'
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                        />
+                    </InputGroup>
+                    <DisplayError errors={errors} touched={touched} field={'profileName'}/>
+                </Form.Group>
+
+                <Form.Group controlId={'profilePassword'}>
+                    <Form.Label>Password</Form.Label>
+                    <InputGroup>
+                        <InputGroup.Text>
+                            Please insert password
+                        </InputGroup.Text>
+                        <FormControl
+                            // className="form-control"
+                            name='profilePassword'
+                            type='password'
+                            value={values.profilePassword}
+                            placeholder='P@ssword!'
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                        />
+                    </InputGroup>
+                    <DisplayError errors={errors} touched={touched} field={'profilePassword'}/>
+                </Form.Group>
+
+                <Form.Group controlId={'profilePasswordConfirm'}>
+                    <Form.Label>Password Confirm</Form.Label>
+                    <InputGroup>
+                        <InputGroup.Text>
+                            Please confirm password
+                        </InputGroup.Text>
+                        <FormControl
+                            // className="form-control"
+                            name='profilePasswordConfirm'
+                            type='password'
+                            value={values.profilePasswordConfirm}
+                            placeholder='P@ssword!Confirm'
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                        />
+                    </InputGroup>
+                    <DisplayError errors={errors} touched={touched} field={'profilePasswordConfirm'}/>
+                </Form.Group>
+
+            </Form>
+            <DisplayStatus status={status} />
+        </>
+    )
 }
