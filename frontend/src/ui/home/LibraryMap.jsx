b6205e1fb037c2a71dc59de0dc471cfb71d31bce
Nycole Davila <ndavila1@cnm.edu>
2022-11-30 21:00:30 -0700

    It works

--- frontend/src/ui/home/LibraryMap.jsx
+++ frontend/src/ui/home/LibraryMap.jsx
@@ -9,20 +9,16 @@ import {Link} from "react-router-dom";
 export function LibraryMap (){
 	//TODO: figure out how to feed the lat/lng for all libraries
 	//TODO: Do we need a get library by lat/lng? Yes on second pass of the DB
-	const libraries = useSelector(state => state.libraries ? state.libraries : [])
+	const libraries = useSelector(state => state.libraries ?? [])
 	const dispatch = useDispatch()
 	const initialEffects = () => {
 		dispatch(fetchAllLibraries())
 	}
-	useEffect(initialEffects, [dispatch])
-	const [show, setShow] = useState(false);
-
-	const handleClose = () => setShow(false);
-	const handleShow = () => setShow(true);
+	React.useEffect(initialEffects, [dispatch])
 
 	return (
 		<>
-			<Container className={'justify-content-center'} fluid={true}>
+			<div className={'justify-content-center'}>
 				<Map initialViewState={{
 					latitude: 35.18,
 					longitude: -106.49,
@@ -32,38 +28,10 @@ export function LibraryMap (){
 				            style={{width: 600, height: 400}}
 				            mapStyle='mapbox://styles/mapbox/satellite-streets-v12'
 				>
-					{libraries.map(library =><MapPin lat={library.libraryLat} lng={library.libraryLng} key={library.libraryId} onClick ={handleShow}/>,
-						<Modal show={show} onHide={handleClose}>
-							<Modal.Body>
-								<Container className={"pt-3"}>
-									<Row className={"d-flex justify-content-center text-center"}>
-										<img className={"img-fluid"} src={"https://img.texasmonthly.com/2016/09/8403741198_e94c0c64c5_k.jpg?auto=compress&crop=faces&fit=fit&fm=jpg&h=0&ixlib=php-3.3.1&q=45&w=1250"} alt={"library pin/book"} />
-										<p>INSERT THE LIBRARY NAME HERE FROM THE DATABASE</p>
-									</Row>
-									<Row>
-										<Col className={"d-flex justify-content-around"}>
-											<Button className={"d-flex justify-content-center"} >Library Details</Button>
-										</Col>
-									</Row>
-								</Container>
-								<Container className={"border border-1 border-dark my-2 p-2"}>
-									<Row className={"text-center"}>
-										<p>EMBED DESCRIPTION HERE</p>
-									</Row>
-									<Row className={"text-center"}>
-										<p>EMBED NEXT EVENT HERE</p>
-									</Row>
-								</Container>
-								<Container className={"pb-2 d-flex justify-content-around"}>
-									<Button >Comment</Button>
-									<Button >Quick Check-In</Button>
-								</Container>
-							</Modal.Body>
-						</Modal>
-
-					)}
+					{libraries.map((libraries, index)=>
+							<MapPin libraries={libraries} index={index} key={index} />)}
 				</Map>
-			</Container>
+			</div>
 		</>
 	)
 }
--- frontend/src/ui/home/MapPin.jsx
+++ frontend/src/ui/home/MapPin.jsx
@@ -1,18 +1,23 @@
-import {Marker} from "react-map-gl";
+import {Marker, Popup} from "react-map-gl";
+import {useState} from "react";
 
 
 // TODO add mapPinModal to Pin and change Pin to our icon
 export function MapPin(props){
-	const {lat, lng, index} = props
+	const{libraries: {libraryLat:lat, libraryLng:lng, libraryName, libraryAddress, libraryDescription, librarySpecialization, libraryType}, index} = props
 
 	const ICON = `M20.2,15.7L20.2,15.7c1.1-1.6,1.8-3.6,1.8-5.7c0-5.6-4.5-10-10-10S2,4.5,2,10c0,2,0.6,3.9,1.6,5.4c0,0.1,0.1,0.2,0.2,0.3
   c0,0,0.1,0.1,0.1,0.2c0.2,0.3,0.4,0.6,0.7,0.9c2.6,3.1,7.4,7.6,7.4,7.6s4.8-4.5,7.4-7.5c0.2-0.3,0.5-0.6,0.7-0.9
   C20.1,15.8,20.2,15.8,20.2,15.7z`;
 
 	const SIZE = 20;
-
+	const [showPopup, setShowPopup] = useState(false)
 	return (
-		<Marker key={`marker-${index}`} longitude={lng} latitude={lat}>
+		<>
+		<Marker key={`marker-${index}`} longitude={lng} latitude={lat} onClick={(e)=>{
+			setShowPopup(!showPopup)
+			e.originalEvent.stopPropagation()
+		}}>
 			<svg
 				height={SIZE}
 				viewBox = "0 0 24 24"
@@ -26,5 +31,18 @@ export function MapPin(props){
 				<path d={ICON} />
 			</svg>
 		</Marker>
+
+	{showPopup && (<Popup longitude={lng} latitude={lat} key={index} onClose={()=> setShowPopup(false)} offset={30} className={'libraryPopup'}>
+		<div>
+			<h2>{libraryName}</h2>
+			<p>{libraryAddress}</p>
+			<p>{libraryDescription}</p>
+			<p>{librarySpecialization}</p>
+			<p>{libraryType}</p>
+		</div>
+		</Popup>
+		)}
+	</>
+
 	)
 }
