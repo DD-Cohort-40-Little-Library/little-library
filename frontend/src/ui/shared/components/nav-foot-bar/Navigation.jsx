9df42c802ebf88c2b82b4a7e9e4084a43f575f37
Kelly Bowman <KBowman5@CNM.Edu>
2022-12-02 21:01:37 -0700

    worked on Nav bar

--- frontend/src/ui/shared/components/nav-foot-bar/Navigation.jsx
+++ frontend/src/ui/shared/components/nav-foot-bar/Navigation.jsx
@@ -2,20 +2,20 @@ import React from "react";
 import {Container, Nav, Navbar, NavDropdown, Button} from "react-bootstrap";
 import {Link} from "react-router-dom";
 import styles from "./nav-footer.module.css";
-//PUT IN OUR LLL LOGO ONCE COMPLETE
-import LLLLogo from "../../";
+import LLLLogo from "../../../../../images/logoAssetsSVG/logoBase-xxs.svg";
+import LLLLabel from "../../../../../images/logoAssetsSVG/lLabel-xs.svg"
 import {SignInSignUpModal} from "../log-in-modals/SignInSignUp.jsx";
 
 export function Navigation() {
     return (
-        <Navbar className={styles.navFootBgColor} expand='xxl'>
+        <Navbar className={styles.navFootBgColor}>
             <Container className={"d-flex justify-content-between"}>
-                <img src={LLLLogo} alt={'Little Library Locator Logo'}/>
-                <Navbar.Brand className={'fs-1 justify-content-center ps-5'} href="/">Little Library Locator</Navbar.Brand>
+                <img src={LLLLogo} className={styles.lLLLogoBase} alt={'Little Library Locator Logo'}/>
+                <img src={LLLLabel} className={styles.LLLLabel} />
                  <Nav>
-                        <Container>
-                            <SignInSignUpModal id="signInSignUpModal"/>
-                        </Container>
+                    <Container>
+                        <SignInSignUpModal id="signInSignUpModal"/>
+                    </Container>
                  </Nav>
             </Container>
         </Navbar>
