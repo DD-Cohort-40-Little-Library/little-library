4aadaad69e78189763caae5a785418185e9ce039
Bryce Roth <broth7@cnm.edu>
2022-12-05 19:25:30 -0700

    got the date to render

--- frontend/src/ui/home/EventShortListing.jsx
+++ frontend/src/ui/home/EventShortListing.jsx
@@ -4,35 +4,20 @@ import {useSelector} from "react-redux";
 
 export const EventShortListing = ({event, library}) => {
 
-	// const dispatch = useDispatch()
-
-	// const event = useSelector(state => {
-	// 	if (state.events[event.eventId] === undefined) {
-	// 		return []
-	// 	} else {
-	// 		return state.events[event.eventId]
-	// 	}
-	// })
-
-
+	const date = event.eventDate
+	const D = new Date(date)
 
 	return(
 		<>
 			<Container className={"border border-dark rounded"}>
 				<Row className={"my-3"}>
-					<Col xs={5}>{library.libraryName}</Col>
-					<Col xs={7}>Library Address</Col>
-				</Row>
-				<Row className={"my-3"}>
-					<Col xs={3}>{event.eventDate}</Col>
-					<Col xs={2}>Event Start</Col>
-					<Col xs={2}>Event End</Col>
-					<Col xs={5}>Event Type</Col>
-				</Row>
-				<Row className={"my-3"}>
-					<Col>
-						{event.eventTitle}
-					</Col>
+					<a xs={5}>{library.libraryName}</a>
+					<a xs={7}>{library.libraryAddress}</a>
+					{/*<a xs={5}>Type of event: {event.eventType}</a>*/}
+					<a xs={5}>Event Title: {event.eventTitle}</a>
+					<a xs={3}>Date: {(D.getMonth() + 1) + '/' + (D.getDate() + '/' + (D.getFullYear()))}</a>
+					{/*<a xs={2}>Start time: {event.eventStart}</a>*/}
+					{/*<a xs={2}>End time: {event.eventEnd}</a>*/}
 				</Row>
 			</Container>
 		</>
