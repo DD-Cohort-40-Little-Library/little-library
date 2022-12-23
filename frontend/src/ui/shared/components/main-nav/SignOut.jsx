f66d4fa411754e6a43a1cc449c752d5bbe06fb22
Nycole Davila <ndavila1@cnm.edu>
2022-12-22 19:26:30 -0700

    sign out modal

--- frontend/src/ui/shared/components/main-nav/SignOut.jsx
+++ frontend/src/ui/shared/components/main-nav/SignOut.jsx
@@ -1,11 +1,15 @@
-import React from "react";
+import React, {useState} from "react";
 import {useDispatch} from "react-redux";
 import {httpConfig} from "../../utils/http-config.js";
 import {getAuth} from "../../../../store/auth.js";
-import {Button} from "react-bootstrap";
+import {Button, Form, InputGroup, Modal, Row} from "react-bootstrap";
 
 export const SignOutComponent = () => {
+    const [show, setShow] = useState(false)
+    const handleClose = () => setShow(false)
+    const handleShow = () => setShow(true)
     const dispatch = useDispatch()
+
     const signOut = () => {
         httpConfig.get('/apis/sign-out/').then(reply => {
 
@@ -14,7 +18,7 @@ export const SignOutComponent = () => {
                 dispatch(getAuth(null))
                 window.location = '/'
             }
-        })
+        });
     }
     return(
         <>
@@ -23,6 +27,20 @@ export const SignOutComponent = () => {
                     Sign Out
                 </Button>
             </div>
+
+            <Modal
+                show={show}
+                onHide={handleClose}
+                backdrop="static"
+                keyboard={false}
+            >
+                    <Row>
+                        <h2 className={"text-center"}>You are now signed out</h2>
+                    </Row>
+                <Modal.Footer>
+                    <Button variant="light" onClick={handleClose}>Close</Button>
+                </Modal.Footer>
+            </Modal>
         </>
     )
 }
