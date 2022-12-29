d1f67b91a95e71caaca90d47f0ed4f82e19e8f4b
BryceRoth22 <roth1116@gmail.com>
2022-12-29 16:55:50 -0700

    in MapCustomPin.jsx, changed line 59 for librarySpecialization to 'Public' so the pins would render correctly

--- frontend/src/ui/home/MapCustomPin.jsx
+++ frontend/src/ui/home/MapCustomPin.jsx
@@ -56,7 +56,7 @@ export function MapCustomPin(props) {
                                                  style={{transform: `translate(${-SIZE / 4}px,${-SIZE}px`}}/>
                 }
 
-                {librarySpecialization === 'Official Community' && <Image src={publicLibrary}
+                {librarySpecialization === 'Public' && <Image src={publicLibrary}
                                                 height={SIZE}
                                                 viewBox="0 0 24 24"
                                                 style={{transform: `translate(${-SIZE / 4}px,${-SIZE}px`}}/>
