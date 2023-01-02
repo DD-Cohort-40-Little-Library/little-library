6e618cf7de8e3d88e3be80b0b3863f2a99ca7365
Nycole Davila <ndavila1@cnm.edu>
2023-01-02 15:23:06 -0700

    main section complete and updates

--- frontend/src/ui/home/Home.jsx
+++ frontend/src/ui/home/Home.jsx
@@ -13,6 +13,8 @@ import homeImageBlk1 from "../../../images/uiSharedImages/homeImgBlk1.jpg";
 import homeImageBlk2 from "../../../images/uiSharedImages/homeImgBlk2.jpg";
 import llStock1 from "../../../images/uiSharedImages/homeLLStock1.jpg";
 import homeImagePins from "../../../images/uiSharedImages/homeImagePins.jpg";
+import lllLogoTag2 from "../../../images/uiSharedImages/mainlllLogoTag.jpg";
+import homeImageBlk4 from "../../../images/uiSharedImages/homeImgBlk4.jpg";
 
 
 export function Home() {
@@ -98,15 +100,20 @@ export function Home() {
 				</Col>
 			</Row>
 			</Container>
+
+
+
 			<Container className={"m-0"} fluid="auto" id={"homeSectionBlk1"}>
 				<Row className={"m-0"}>
 					<img src={homeImageBlk1} alt={"homeImage1"} id={"sectionImageBlk"} className={"img-fluid"}/>
 				</Row>
 			</Container>
 
-
 			<Container className={""} fluid="auto">
 				<Row className={"m-0 "} id={"homeSectionBlk2"}>
+					<Col>
+						<img src={llStock1} alt={"llStock1"} id={"sectionImageBlk"} className={""} style={{width: '20rem'}}/>
+					</Col>
 					<Col>
 						<h1 className={""}>Our Mission</h1>
 						<p>“Our Mission is to foster a social environment around literacy. Through the Little Library Locator, we aim to promote different interests around reading and highlight the unique personalities of the creators of Little Libraries around the world.”</p>
@@ -114,10 +121,26 @@ export function Home() {
 						<h2>Join the project.</h2>
 						<p style={{fontSize: "large"}}>Sign Up and Register Your Little Library</p>
 						<Link to='/library-create'><Button>Add A Library</Button></Link>
+					</Col>
+				</Row>
+			</Container>
 
+			<Container className={"m-0"} fluid="auto" id={"homeSectionBlk6"}>
+				<Row className={"m-0"}>
+					<img src={homeImageBlk4} alt={"homeImage4"} id={"sectionImageBlk"} className={"img-fluid"}/>
+				</Row>
+			</Container>
+
+			<Container className={""} fluid="auto">
+				<Row className={"m-0 "} id={"homeSectionBlk5"}>
+					<Col>
+						<h1 className={""}>Who We Are</h1>
+						<p>Four people from different worlds came together and built this site. A couple with some coding exposure and the others with none. Within ten weeks of intense coding experience, we became a great team. Together, we have created a great application had a fantastic time doing it!</p>
+						<h3>Meet the Team</h3>
+						<Link to='/about-us'><Button>About Us</Button></Link>
 					</Col>
 					<Col>
-						<img src={llStock1} alt={"llStock1"} id={"sectionImageBlk"} className={""} style={{width: '20rem'}}/>
+						<img src={lllLogoTag2} alt={"lllLogoTag"} id={"sectionImageBlk"} className={""} style={{width: '30rem'}}/>
 					</Col>
 				</Row>
 			</Container>
