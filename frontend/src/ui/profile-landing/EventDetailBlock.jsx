12c558635bf8397b408e720daf24bc44284d6225
BryceRoth22 <roth1116@gmail.com>
2023-01-02 15:00:14 -0700

    added more to the event tab on profile landing page

--- frontend/src/ui/profile-landing/EventDetailBlock.jsx
+++ frontend/src/ui/profile-landing/EventDetailBlock.jsx
@@ -1,29 +1,45 @@
-import {Container, Row} from "react-bootstrap";
+import {Button, Col, Container, Row} from "react-bootstrap";
 import React from "react";
 
 export const EventDetailBlock = ({event, library}) => {
 
     const date = event.eventDate
     const D = new Date(date)
-    const time = event.eventStart
-    const startTime = new Date(time)
-    let hour = ((startTime.getHours() + 7) % 12) || 12
-    let minutes = startTime.getMinutes()
+    const timeS = event.eventStart
+    const startTime = new Date(timeS)
+    let hourS = ((startTime.getHours() + 7) % 12) || 12
+    let minutesS = startTime.getMinutes()
     if (startTime.getMinutes() === 0) {
-        minutes = minutes + '0'
+        minutesS = minutesS + '0'
     }
-    const amPm = startTime.getHours() > 12 ? 'AM' : 'PM'
-    const finalTime = (hour + ":" + minutes + amPm)
+    const amPmS = startTime.getHours() > 12 ? 'AM' : 'PM'
+    const finalStartTime = (hourS + ":" + minutesS + amPmS)
+
+    const timeE = event.eventEnd
+    const endTime = new Date(timeE)
+    let hourE = ((endTime.getHours() + 7) % 12) || 12
+    let minutesE = endTime.getMinutes()
+    if (endTime.getMinutes() === 0) {
+        minutesE = minutesE + '0'
+    }
+    const amPmE = endTime.getHours() > 12 ? 'AM' : 'PM'
+    const finalEndTime = (hourE + ":" + minutesE + amPmE)
 
     return(
         <>
             <Container className={"border border-dark rounded"}>
-                <Row xs={5}>Title: {event.eventTitle}</Row>
-                <Row xs={5}>Date: {(D.getMonth() + 1) + '-' + ((D.getDate() + 1) + '-' + (D.getFullYear()))}</Row>
-                <Row xs={5}>Start time: {finalTime}</Row>
-                <Row xs={6}>Library Name: {library.libraryName}</Row>
-                <Row xs={6}>Library Address: {library.libraryAddress}</Row>
-                <Row xs={7}>Description: {event.eventDescription}</Row>
+                <Col>
+                    <Row xs={6}>Library Name: {library.libraryName}</Row>
+                    <Row xs={6}>Library Address: {library.libraryAddress}</Row>
+                    <Row xs={5}>Event: {event.eventTitle}</Row>
+                    <Row xs={5}>Date: {(D.getMonth() + 1) + '-' + ((D.getDate() + 1) + '-' + (D.getFullYear()))}</Row>
+                    <Row xs={5}>Start time: {finalStartTime}</Row>
+                    <Row xs={5}>End time: {finalEndTime}</Row>
+                    <Row xs={7}>Description: {event.eventDescription}</Row>
+                </Col>
+                <Col>
+                    <Button>Go To This Library</Button>
+                </Col>
             </Container>
         </>
     )
